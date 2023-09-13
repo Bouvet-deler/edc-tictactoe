7eb60ca38041490412b8259312427795b2278800
Mob1 <User@DESKTOP-SHMCF0D>
2023-09-13 14:43:49 +0200

    Extracted InitBoard

--- tictactoe/Game.cs
+++ tictactoe/Game.cs
@@ -10,17 +10,23 @@
     public class Board //TODO: Large class (uncohesive), correct data structure??
     {
         private List<Tile> _plays = new List<Tile>(); //TODO: Bad naming, is this the correct data structure to use?
-
+        private const int BOARDSIZE = 3;
         public Board()
         {
-            for (int i = 0; i < 3; i++) //TODO: Readability, extract? Magic number (also on line below)
+            InitBoard();
+        }
+
+        private void InitBoard()
+        {
+            for (int i = 0; i < BOARDSIZE; i++) //TODO: Readability, extract? Magic number (also on line below)
             {
-                for (int j = 0; j < 3; j++)
+                for (int j = 0; j < BOARDSIZE; j++)
                 {
                     _plays.Add(new Tile { X = i, Y = j, Symbol = ' ' });
                 }
             }
         }
+
         public Tile TileAt(int x, int y)
         {
             return _plays.Single(tile => tile.X == x && tile.Y == y);
